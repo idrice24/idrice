8ded75f5b450cd0937443b999c2924f75dcf3d12
idrice24 <idrice.tsafouet@yahoo.com>
2022-09-20 21:11:18 +0100

    Create admin.module.ts

---
+++ src/app/admin/admin.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { AdminComponent } from './components/admin/admin.component';
+import { ManageBlogComponent } from './components/manage-blog/manage-blog.component';
+import { ManageGalleryComponent } from './components/manage-gallery/manage-gallery.component';
+import { ManageProductComponent } from './components/manage-product/manage-product.component';
+import { ManageUserComponent } from './components/manage-user/manage-user.component';
+import { ManageVideoComponent } from './components/manage-video/manage-video.component';
+import { ManageAbonnementComponent } from './components/manage-abonnement/manage-abonnement.component';
+
+
+
+@NgModule({
+  declarations: [
+    LoginComponent,
+    RegisterComponent,
+    AdminComponent,
+    ManageBlogComponent,
+    ManageGalleryComponent,
+    ManageProductComponent,
+    ManageUserComponent,
+    ManageVideoComponent,
+    ManageAbonnementComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class AdminModule { }
